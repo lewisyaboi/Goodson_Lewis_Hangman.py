11a51d52eebe1a20db1d442c4a3de75df9ef4bd0
lewisyaboi <93371258+lewisyaboi@users.noreply.github.com>
2021-10-29 09:43:20 +0100

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,4 +1,5 @@
 import random
+import time
 file = open("word_list.txt")
 content = file.readlines()
 new_content = [x[:-1] for x in content]
@@ -8,30 +9,30 @@ length = len(word)
 lives = 7
 blanks = '*' * length
 letters = 0
+lists = ['']
 win = False
 
 while True:
     print(blanks, '\t\tLives: ', lives, '\n')
     guess = input('Please enter your next guess:')
 
-    if guess in blanks:
+    if guess in lists:
         print('Letter already guessed\n')
-
-    elif guess in word:
-        print ('Correct!\n')
-        letters = letters + 1
-
-    elif guess not in word:
-        print ('Wrong!\n')
-        lives = lives - 1
-        if lives == 0:
-            break
-
+    else:
+        if guess in word:
+            print ('Correct!\n')
+            letters = letters + 1
+        if guess not in word:
+            print ('Wrong!\n')
+            lives = lives - 1
+    lists.append(guess)
+    
     for i in range(len(word)):  # replace blanks with correct guessed letters
         if word[i] in guess:
             blanks = blanks[:i] + word[i] + blanks[i+1:]
 
-    if letters == length:
+    if lives == 0:
+        print('The original word was:', word)
         print('You Lose')
         break
     if '*' not in blanks:
